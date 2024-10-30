3f879fd8cd010195ba1ac43f32f244c555c9d13f
Mike <barbeque@users.noreply.github.com>
2024-10-29 22:12:28 -0600

    Add names to masks

--- firmware/p6_cart.c
+++ firmware/p6_cart.c
@@ -9,6 +9,7 @@
 
 #define ALL_GPIO_MASK   	0x3FFFFFFF // 30 IOs 0..29 inclusive
 #define ADDR_GPIO_MASK  	0x0003fffc // GPIO 2 .. GPIO 17 inclusive, GPIO 0 and GPIO 1 off
+#define ROMADDR_GPIO_MASK   0x0000fffc // Just the addresses in an 8K ROM
 #define CS_GPIO_MASK        0x00040000 // GPIO 18 lights up when CS2 or CS3 are selected
 #define DATA_GPIO_MASK  	0x07f80000 // GPIO 19 .. GPIO 26 inclusive (0xff << 19)
 
@@ -40,9 +41,10 @@ int __not_in_flash_func(emulate_boot_rom)() {
             // Totally normal cartridge ROM behaviour.
             SET_DATA_MODE_OUT;
 
-            //addr = (pins & ADDR_GPIO_MASK) >> 2; // shift down so it starts at 0
-            addr = 0; // HACK
+            addr = (pins & ROMADDR_GPIO_MASK) >> 2; // shift down so it starts at 0
+            //addr = 0; // HACK
             // shifted by number of address pins (and also the ESP pins on 0, 1 and GPIO18 for ~CS) - GPIO0 to 18 inclusive = 19 pins
+            // address limited to 0x3fff (8k) because otherwise a14 and a15 from the decode will interfere
             gpio_put_masked(DATA_GPIO_MASK, ((uint32_t)(P6_bootrom[addr])) << 19);
 
             // wait for select to release (go high)
